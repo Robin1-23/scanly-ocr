844d8a6b27c2492f9aad2ea47ba553d3bf9ebf30
robin0556 <roobinxyz@gmail.com>
2025-04-09 17:30:18 +0530

    updated config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,48 @@
-// next.config.js
+// // next.config.js
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    urlImports: ['https://*'],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /pdf\.worker(\.min)?\.js$/,
-      loader: 'worker-loader',
-      options: {
-        filename: 'static/[hash].worker.js',
-      },
-    });
-    return config;
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     urlImports: ['https://*'],
+//   },
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /pdf\.worker(\.min)?\.js$/,
+//       loader: 'worker-loader',
+//       options: {
+//         filename: 'static/[hash].worker.js',
+//       },
+//     });
+//     return config;
+//   },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
 
 
 
 // export default nextConfig;
+
+
+
+
+
+import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
+
+const nextConfig: NextConfig = {
+  experimental: {
+    urlImports: ['https://*'],
+  },
+  webpack(config: Configuration) {
+    config.module?.rules?.push({
+      test: /pdf\.worker(\.min)?\.js$/,
+      loader: 'worker-loader',
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
+
