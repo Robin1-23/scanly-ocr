6920342afec9b5f26dd27bc6e555c229f89c3b7a
robin0556 <roobinxyz@gmail.com>
2025-04-09 17:35:54 +0530

    agin update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,7 @@
-// // next.config.js
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     urlImports: ['https://*'],
-//   },
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /pdf\.worker(\.min)?\.js$/,
-//       loader: 'worker-loader',
-//       options: {
-//         filename: 'static/[hash].worker.js',
-//       },
-//     });
-//     return config;
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
-
-// export default nextConfig;
-
-
-
-
-
 import type { NextConfig } from 'next';
 import type { Configuration } from 'webpack';
 
+/** @type {NextConfig} */
 const nextConfig: NextConfig = {
   experimental: {
     urlImports: ['https://*'],
@@ -38,6 +10,9 @@ const nextConfig: NextConfig = {
     config.module?.rules?.push({
       test: /pdf\.worker(\.min)?\.js$/,
       loader: 'worker-loader',
+      options: {
+        filename: 'static/[hash].worker.js',
+      },
     });
 
     return config;
@@ -45,4 +20,5 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
 
